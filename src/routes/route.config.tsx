d0455b37a0253e4a2079d4c938ad82a561cb85a6
Ayooluwa Aduwo <aduwoayooluwa@gmail.com>
2024-09-02 13:52:40 +0100

    fix: routing issue

--- src/routes/route.config.tsx
+++ src/routes/route.config.tsx
@@ -1,119 +1,34 @@
 import { createBrowserRouter } from "react-router-dom";
+import MainLayout from '../App'
 import LoginPage from "../pages/auth/login";
-// import AddJob from "../pages/dashboard/add-job";
-import JobDetailsPage from "../pages/dashboard/job-details";
-import EmailTemplatesPage from "../pages/dashboard/email-templates";
-import RemindersPage from "../pages/dashboard/reminders";
 import Dashboard from "../pages/dashboard/dashboard";
-import Navbar from "../layout/nav-bar";
+import HeroSection from "../pages/home";
 import AddCalendarPage from "../pages/dashboard/add-calendar";
 import EmailForm from "../pages/dashboard/compose-email";
 import AddContact from "../pages/dashboard/add-contact";
 import AddMeeting from "../pages/dashboard/add-meeting";
-import HeroSection from "../pages/home";
-// import { NylasSchedulerEditor, NylasScheduling } from "@nylas/react";
+import JobDetailsPage from "../pages/dashboard/job-details";
+import EmailTemplatesPage from "../pages/dashboard/email-templates";
+import RemindersPage from "../pages/dashboard/reminders";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: (
-      <div>
-        <Navbar />
-        <HeroSection />
-      </div>
-    ),
+    element: <MainLayout />, 
+    children: [
+      { path: "/", element: <HeroSection /> },
+      { path: "/add-job", element: <AddCalendarPage /> },
+      { path: "/compose-email", element: <EmailForm /> },
+      { path: "/add-contact", element: <AddContact /> },
+      { path: "/add-meeting", element: <AddMeeting /> },
+      { path: "/job-details/:id", element: <JobDetailsPage /> },
+      { path: "/email-templates", element: <EmailTemplatesPage /> },
+      { path: "/reminders", element: <RemindersPage /> },
+      { path: "/dashboard", element: <Dashboard /> },
+    ],
   },
-
   {
     path: "/login",
-    element: (
-      <div>
-        <LoginPage />
-      </div>
-    ),
-  },
-  {
-    path: "/add-job",
-    element: (
-      <div>
-        <Navbar />
-        <AddCalendarPage />
-      </div>
-    ),
-  },
-  {
-    path: "/compose-email",
-    element: (
-      <div>
-        <Navbar />
-        <EmailForm />
-      </div>
-    ),
-  },
-  {
-    path: "/add-contact",
-    element: (
-      <div>
-        <Navbar />
-        <AddContact />
-      </div>
-    ),
-  },
-
-  {
-    path: "/add-meeting",
-    element: (
-      <div>
-        <Navbar />
-        <AddMeeting />
-      </div>
-    ),
-  },
-
-  //   {
-  //     path: "/add-calendar",
-  //     element: (
-  //       <div>
-  //         <Navbar />
-  //         <AddJob />
-  //       </div>
-  //     ),
-  //   },
-
-  {
-    path: "/job-details/:id",
-    element: (
-      <div>
-        <Navbar />
-        <JobDetailsPage />
-      </div>
-    ),
-  },
-  {
-    path: "/email-templates",
-    element: (
-      <div>
-        <Navbar />
-        <EmailTemplatesPage />
-      </div>
-    ),
-  },
-  {
-    path: "/reminders",
-    element: (
-      <div>
-        <Navbar />
-        <RemindersPage />
-      </div>
-    ),
-  },
-  {
-    path: "/dashboard",
-    element: (
-      <div>
-        <Navbar />
-        <Dashboard />
-      </div>
-    ),
+    element: <LoginPage />,
   },
 ]);
